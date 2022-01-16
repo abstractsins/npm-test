d89a57389f0cb08f8e60fe1d1a1c1a73246effe2
Dan The Man <18626012+abstractsins@users.noreply.github.com>
2022-01-15 20:53:08 -0500

    install chai

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 console.log('hello world!')
+
+// TEST OBJECT
+const myHouse = {
+    address: "828",
+    floors: 2,
+    sqft: 800,
+    bathrooms: 1,
+    style: 'loft',
+    heat: 'electric',
+    gas: false,
+    cable: false,
+    internet: 'XFinity'
+}
+
+// EXPORTS
+module.exports = myHouse;
--- index_test.js
+++ index_test.js
@@ -1,12 +1,27 @@
 const assert = require('assert');
+const myHouse = require('./index');
 
-describe('Test this thing', () => {
-    describe('actually this', () => {
-        it('should add things', () => {
-            const predicted = 3 + 4;
-            const actual = 7;
-
+describe('My House', () => {
+    describe('its properties', () => {
+        it('the address should be a string', ()=> {
+            const predicted = 'string';
+            const actual = typeof(myHouse.address);
+            assert.strictEqual(actual, predicted);
+        });
+        it('the address should be correct', () => {
+            const predicted = "828";
+            const actual = myHouse.address;
+            assert.strictEqual(actual, predicted);
+        });
+        it('should have 2 floors', () => {
+            const predicted = 2;
+            const actual = myHouse.floors;
             assert.strictEqual(actual, predicted);
         })
+        it('should have electric heat', () => {
+            const predicted = 'electric';
+            const actual = myHouse.heat;
+            assert.strictEqual(actual, predicted);
+        });
     });
 })
